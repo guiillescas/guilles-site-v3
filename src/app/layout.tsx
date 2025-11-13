62a79dda5433eff3448c1336e19742039fda72ba
Guilherme Illescas <gui.illescas@icloud.com>
2025-11-13 13:16:24 -0300

    feat: added analytics to site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Roboto_Flex } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -177,6 +179,8 @@ export default function RootLayout({
       >
         {children}
       </body>
+
+      <Analytics />
     </html>
   );
 }
